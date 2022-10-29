6d0a9222cde9e1aaf365fd0e40e1fee11c4e804b
Jan Stepien <jasiek.stepien@gmail.com>
2022-10-29 09:49:28 +0200

    init: initialize project

---
+++ android/app/src/main/kotlin/com/example/hot_movies/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hot_movies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hot_movies/app/app.dart';
+
+void main(List<String> args) {
+  runApp(const App());
+}
